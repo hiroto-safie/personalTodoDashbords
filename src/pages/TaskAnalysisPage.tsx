cbc77c26da55f725b3639554842595d6a042ebcd
hiroto-safie <h-sasaki@safie.jp>
2024-08-02 19:03:09 +0900

    add: Page title added

--- src/pages/TaskAnalysisPage.tsx
+++ src/pages/TaskAnalysisPage.tsx
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
 const TaskAnalysisPage: React.FC = () => {
-    return <div>TaskAnalysisPage</div>;
+    return (
+        <>
+            <Typography variant="h2">Task Analysis</Typography>
+        </>
+    )
 };
 
 export default TaskAnalysisPage;
--- src/pages/TasklistPage.tsx
+++ src/pages/TasklistPage.tsx
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
 const TasklistPage: React.FC = () => {
-    return <div>TasklistPage</div>;
+    return (
+        <>
+            <Typography variant="h2">Task List</Typography>
+        </>
+    )
 };
 
 export default TasklistPage;
