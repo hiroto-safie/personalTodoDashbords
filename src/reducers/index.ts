d28b526314efddf7e6fe5257b1bc7a828620bbed
hiroto-safie <h-sasaki@safie.jp>
2024-08-13 17:29:13 +0900

    fix: reducers codes are removed and updated, because my using way was wrong

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,33 @@
-import { combineReducers } from "@reduxjs/toolkit";
+import { Action } from "../types/action";
+import { ADD_TASK, EDIT_TASK, DELETE_TASK } from "../actions";
+import { State } from "../types/state";
+import { sampleTasks } from "../services/task";
 
-import { addTaskReducer } from "./addTaskReducer";
-import { deleteTaskReducer } from "./deleteTaskReducer";
-import { editTaskReducer } from "./editTaskReducer";
+const initialState: State = {
+    tasks: [
+        ...sampleTasks,
+    ],
+    nextId: 1
+}
 
-export const rootReducer = combineReducers({
-    addTaskReducer,
-    deleteTaskReducer,
-    editTaskReducer
-})
+export const rootReducer = (state = initialState, action: Action): State => {
+    switch (action.type) {
+        case ADD_TASK:
+            return {
+                ...state,
+                tasks: [...state.tasks, {...action.payload, id: state.nextId}],
+                nextId: state.nextId + 1
+            }
+        case EDIT_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.map(task => task.id === action.payload.id ? action.payload : task)
+        }
+        case DELETE_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.filter(task => task.id !== action.payload)
+            }
+        default:
+            return state
+}}
