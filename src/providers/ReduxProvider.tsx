0ed57c5672bb006893fd8e8fb20951852fba1c62
hiroto-safie <h-sasaki@safie.jp>
2024-08-13 12:07:21 +0900

    add store.ts file was added

--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -1,22 +1,6 @@
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "../reducers";
+import store from "../stores";
 import React from "react";
-
-const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-          serializableCheck: {
-            // Ignore these action types
-            ignoredActions: ['your/action/type'],
-            // Ignore these field paths in all actions
-            ignoredActionPaths: ['meta.arg', 'payload.dueDate'],
-            // Ignore these paths in the state
-            ignoredPaths: ['items.dates'],
-        },
-    }),
-})
 
 export const ReduxProvider: React.FC<{children: JSX.Element}> = ({children}) => {
     return <Provider store={store}>{children}</Provider>;
